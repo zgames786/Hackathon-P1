ac858b0353668936e409001ecea711cf2540d543
Ziad Siddiqui <ziadsiddiqui@gmail.com>
2025-11-28 23:19:53 -0500

    login

--- public/firebase-init.js
+++ public/firebase-init.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDDaOx0GXBrDOx_-NBUWQVQXbYfutuclWQ",
-    authDomain: "thna-school-app.firebaseapp.com",
-    projectId: "thna-school-app",
-    storageBucket: "thna-school-app.appspot.com",
-    messagingSenderId: "777172524006",
-    appId: "1:777172524006:web:7284bdc57050df287bd427",
-    measurementId: "G-B1E8WXYR9V"
-    };
+  apiKey: "AIzaSyDDaOx0GXBrDOx_-NBUWQVQXbYfutuclWQ",
+  authDomain: "thna-school-app.firebaseapp.com",
+  projectId: "thna-school-app",
+  storageBucket: "thna-school-app.appspot.com",
+  messagingSenderId: "777172524006",
+  appId: "1:777172524006:web:7284bdc57050df287bd427",
+  measurementId: "G-B1E8WXYR9V"
+};
     
     firebase.initializeApp(firebaseConfig);
     
